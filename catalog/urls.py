f26f9f73cce6217c7799d590cce1661ffd9de937
PavelZikol <142353611+PavelZikol@users.noreply.github.com>
2024-05-29 11:01:59 +0400

    fixed

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import products, description
+from catalog.views import products, description, main, main_3
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', products, name='product'),
+    path('main/', main, name='main'),
+    path('main/', main_3, name='main_3'),
     path('<int:pk>/catalog/', description, name='description'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,15 +4,30 @@ from catalog.models import Product
 # Create your views here.
 def products(request):
     context = {
-        'object_list' : Product.objects.all(),
+        'object_list': Product.objects.all()[:3],
         'title': 'Продукты'
     }
     return render(request, 'catalog/product_list.html', context=context)
 
+def main(request):
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Продукты-главная'
+    }
+    return render(request, 'catalog/main.html', context=context)
+
+def main_3(request):
+    context = {
+        'object_list': Product.objects.all()[:3],
+        'title': 'Продукты-главная'
+    }
+    return render(request, 'catalog/main.html', context=context)
+
+
 def description(request,pk):
     category_item = Product.objects.get(pk=pk)
     context = {
-        'object_list' : Product.objects.filter( pk=pk),
+        'object_list': Product.objects.filter(pk=pk),
         'title': 'Продукты'
     }
     return render(request, 'catalog/description.html', context=context)
